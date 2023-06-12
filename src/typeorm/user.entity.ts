4d5e528de5ffbc3e7562af756fd47d049ca2195f
artemkolodko <temakolodko@gmail.com>
2023-06-12 19:52:38 +0100

    Add user db entity

---
+++ src/typeorm/user.entity.ts
@@ -0,0 +1,55 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum, IsString } from 'class-validator';
+
+export enum AppName {
+  telegram = 'telegram',
+}
+
+@Entity({ name: 'user' })
+export class UserEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ApiProperty()
+  @IsEnum(AppName)
+  @Column({
+    type: 'varchar',
+    enum: AppName,
+  })
+  appName: AppName;
+
+  @ApiProperty()
+  @Column({
+    type: 'varchar',
+    default: '',
+  })
+  userId: string;
+
+  @ApiProperty()
+  @IsString()
+  @Column({
+    type: 'varchar',
+    default: '',
+  })
+  userAddress: string;
+
+  @IsString()
+  @Column({
+    type: 'varchar',
+    default: '',
+  })
+  privateKey: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+}
